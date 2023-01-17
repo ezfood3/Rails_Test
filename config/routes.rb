ef3e111d76d3d99f8cfb06b7b48415cd78c0497a
Donggeun <ezfood3@naver.com>
2023-01-17 16:13:30 +0900

    Ruby on rails

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  get '/posts/index'
+
+  get '/posts/new'
+
+  post '/posts/create'
+
+  get '/posts/destroy/:post_id' => 'posts#destroy'
+
+  get 'posts/edit/:post_id' => 'posts#edit'
+
+  post 'posts/update/:post_id' => 'posts#update'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
